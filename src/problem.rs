049d95253c29b3475c78e0a480e85bca0420134d
Srujun Thanmay Gupta <srujun@srujun.com>
2019-12-06 16:31:17 -0600

    Initial commit with problem.rs trait.

---
+++ src/problem.rs
@@ -0,0 +1,9 @@
+pub trait Problem {
+  fn part_one(&self, input: &str) -> String {
+    "Part one not yet implemented.".to_string()
+  }
+
+  fn part_two(&self, input: &str) -> String {
+    "Part two not yet implemented.".to_string()
+  }
+}
